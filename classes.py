21ea73260e11995ca0500f690def15b248393ad3
DarthKitten <darthkitten2228@gmail.com>
2024-10-19 15:27:27 +0530

    removed pieces dict

--- classes.py
+++ classes.py
@@ -2,6 +2,7 @@ import pygame as pg
 
 class Piece(pg.sprite.Sprite):
     def __init__(self, x, y, color, type,pos):
+        super().__init__()
         self.x = x
         self.y = y
         self.color = color
@@ -9,41 +10,13 @@ class Piece(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x * 180
         self.rect.y = y * 180
+        self.pos = pos
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
-pieces = {
-        "brook1": Piece(0, 0, "black", "rook",'a8'),
-        "brook2": Piece(7, 0, "black", "rook",'h8'),
-        "bknight1": Piece(1, 0, "black", "knight",'b8'),
-        "bknight2": Piece(6, 0, "black", "knight",'g8'),
-        "bbishop1": Piece(2, 0, "black", "bishop",'c8'),
-        "bbishop2": Piece(5, 0, "black", "bishop",'f8'),
-        "bqueen": Piece(3, 0, "black", "queen",'d8'),
-        "bking": Piece(4, 0, "black", "king",'e8'),
-        "bpawn1": Piece(0, 1, "black", "pawn",'a7'),
-        "bpawn2": Piece(1, 1, "black", "pawn",'b7'),
-        "bpawn3": Piece(2, 1, "black", "pawn",'c7'),
-        "bpawn4": Piece(3, 1, "black", "pawn"'d7'),
-        "bpawn5": Piece(4, 1, "black", "pawn",'e7'),
-        "bpawn6": Piece(5, 1, "black", "pawn",'f7'),
-        "bpawn7": Piece(6, 1, "black", "pawn",'g7'),
-        "bpawn8": Piece(7, 1, "black", "pawn",'h7'),
-        "wrook1": Piece(0, 7, "white", "rook",'a1'),
-        "wrook2": Piece(7, 7, "white", "rook",'h1'),
-        "wknight1": Piece(1, 7, "white", "knight",'b1'),
-        "wknight2": Piece(6, 7, "white", "knight",'g1'),
-        "wbishop1": Piece(2, 7, "white", "bishop",'c1'),
-        "wbishop2": Piece(5, 7, "white", "bishop",'f1'),
-        "wqueen": Piece(3, 7, "white", "queen",'d1'),
-        "wking": Piece(4, 7, "white", "king",'e1'),
-        "wpawn1": Piece(0, 6, "white", "pawn",'a2'),
-        "wpawn2": Piece(1, 6, "white", "pawn",'b2'),
-        "wpawn3": Piece(2, 6, "white", "pawn",'c2'),
-        "wpawn4": Piece(3, 6, "white", "pawn",'d2'),
-        "wpawn5": Piece(4, 6, "white", "pawn",'e2'),
-        "wpawn6": Piece(5, 6, "white", "pawn",'f2'),
-        "wpawn7": Piece(6, 6, "white", "pawn",'g2'),
-        "wpawn8": Piece(7, 6, "white", "pawn",'h2')
-    }
+    def Update(self,events):
+        for event in events:
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if self.rect.collidepoint(event.pos):
+                    print("wow")
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame as pg
-from classes import pieces
+from classes import Piece
 
 def boardInit(screen,grid):
     tileSize = 180
@@ -13,15 +13,16 @@ def boardInit(screen,grid):
                 pg.draw.rect(screen, (255, 255, 255),rect)
             elif (x+y) % 2 == 1:
                 pg.draw.rect(screen, (51, 102, 0), rect)
-
-            grid[letter[x]].append([rect])
+            grid[x][y] = rect
 
 
 def pieceInit(screen,grid):
   
-    for piece in pieces:
-        pieces[piece].draw(screen)
-        grid[piece.pos[0]]
+    for piece in pieces.values():
+        piece.draw(screen)
+        piece.Update(pg.event.get())
+
+        grid[piece.x][piece.y] = piece
 
 
 
@@ -31,7 +32,42 @@ def main():
     pg.init()
 
     screen = pg.display.set_mode((2460, 1600))
-    grid = {'h':[],'g':[],'f':[],'e':[],'d':[],'c':[],'b':[],'a':[]}
+    chessGrid = [{},{},{},{},{},{},{},{}]
+    boardGrid = [{},{},{},{},{},{},{},{}]
+    pieceGroup = pg.sprite.Group()
+    pieceGroup.add(
+         Piece(7, 0, "black", "rook",'h8'),
+         Piece(0, 0, "black", "rook",'a8'),
+         Piece(1, 0, "black", "knight",'b8'),
+         Piece(6, 0, "black", "knight",'g8'),
+         Piece(2, 0, "black", "bishop",'c8'),
+         Piece(5, 0, "black", "bishop",'f8'),
+         Piece(3, 0, "black", "queen",'d8'),
+         Piece(4, 0, "black", "king",'e8'),
+         Piece(0, 1, "black", "pawn",'a7'),
+         Piece(1, 1, "black", "pawn",'b7'),
+         Piece(2, 1, "black", "pawn",'c7'),
+         Piece(3, 1, "black", "pawn",'d7'),
+         Piece(4, 1, "black", "pawn",'e7'),
+         Piece(5, 1, "black", "pawn",'f7'),
+         Piece(6, 1, "black", "pawn",'g7'),
+         Piece(7, 1, "black", "pawn",'h7'),
+         Piece(0, 7, "white", "rook",'a1'),
+         Piece(7, 7, "white", "rook",'h1'),
+         Piece(1, 7, "white", "knight",'b1'),
+         Piece(6, 7, "white", "knight",'g1'),
+         Piece(2, 7, "white", "bishop",'c1'),
+         Piece(5, 7, "white", "bishop",'f1'),
+         Piece(3, 7, "white", "queen",'d1'),
+         Piece(4, 7, "white", "king",'e1'),
+         Piece(0, 6, "white", "pawn",'a2'),
+         Piece(1, 6, "white", "pawn",'b2'),
+         Piece(2, 6, "white", "pawn",'c2'),
+         Piece(3, 6, "white", "pawn",'d2'),
+         Piece(4, 6, "white", "pawn",'e2'),
+         Piece(5, 6, "white", "pawn",'f2'),
+         Piece(6, 6, "white", "pawn",'g2'),
+         Piece(7, 6, "white", "pawn",'h2'))
 
     while True:
         for event in pg.event.get():
@@ -40,8 +76,8 @@ def main():
                 quit()
 
         screen.fill((153, 102, 0))
-        boardInit(screen,grid)
-        pieceInit(screen)
+        boardInit(screen,boardGrid)
+        pieceGroup.draw(screen)
         pg.display.flip()
 
 if __name__ == '__main__':
