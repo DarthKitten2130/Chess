b50c708f1f2b13a42029a6543acd7751e1f2db69
DarthKitten <darthkitten2228@gmail.com>
2024-12-30 00:54:25 +0530

    Created Legal Move for Rook and Bishop

--- classes.py
+++ classes.py
@@ -16,16 +16,15 @@ class Tile(pg.sprite.Sprite):
 
 
 class Piece(pg.sprite.Sprite):
-    movable_tiles = []
     def __init__(self, x, y, color, type):
         super().__init__()
         self.x = x
         self.y = y
         self.color = color
         self.image = pg.image.load(f"images/{color}/{color}_{type}.png")
-        self.rect = self.image.get_rect()
-        self.rect.x = x * 180
-        self.rect.y = y * 180
+        self.rect = pg.Rect(x * Tile.tilesize, y * Tile.tilesize, Tile.tilesize, Tile.tilesize)
+        self.movable_tiles = self.legal_move()
+        self.moved = False
 
     def kill(self,live_pieces,dead_pieces):
         dead_pieces.add(self)
@@ -33,6 +32,9 @@ class Piece(pg.sprite.Sprite):
 
     def outline(self,screen):
         pg.draw.rect(screen, (255, 0, 0), self.rect, 5)
+
+    def legal_move(self,chess_grid):
+        pass
 
 
 class King(Piece):
@@ -43,7 +45,7 @@ class King(Piece):
         raise AttributeError("Kings cannot be killed")
 
     def legal_move(self,chess_grid):
-        lst = super().__getattribute__("movable_tiles")
+        lst = []
         for i in range(self.x-1,self.x+2):
             for j in range(self.y-1,self.y+2):
                 if 0 <= i <= 7 and 0 <= j <= 7 and i != self.x and j != self.y:
@@ -56,21 +58,6 @@ class King(Piece):
     def castle(self,target,chess_grid):
         pass
 
-class Queen(Piece):
-    def __init__(self, x, y, color):
-        super().__init__(x, y, color, "queen")
-
-
-class Bishop(Piece):
-    def __init__(self, x, y, color):
-        super().__init__(x, y, color, "bishop")
-
-
-class Knight(Piece):
-    def __init__(self, x, y, color):
-        super().__init__(x, y, color, "knight")
-
-
 class Rook(Piece):
     def __init__(self, x, y, color):
         super().__init__(x, y, color, "rook")
@@ -78,6 +65,97 @@ class Rook(Piece):
     def castle(self,target,chess_grid):
         pass
 
+    def legal_move(self,chess_grid):
+        lst = []
+        for i in range(self.y,8):
+            if chess_grid[i][self.y]:
+                if chess_grid[i][self.y].color != self.color:
+                    lst.append([i,self.y])
+                break
+            else:
+                lst.append([i,self.y])
+
+        for i in range(self.y,0,-1):
+            if chess_grid[i][self.y]:
+                if chess_grid[i][self.y].color != self.color:
+                    lst.append([i, self.y])
+                break
+            else:
+                lst.append([i,self.y])
+
+        for j in range(self.x,8):
+            if chess_grid[self.x][j]:
+                if chess_grid[self.x][j].color != self.color:
+                    lst.append([self.x,j])
+                break
+            else:
+                lst.append([self.x,j])
+
+        for j in range(self.x,0,-1):
+            if chess_grid[self.x][j]:
+                if chess_grid[self.x][j].color != self.color:
+                    lst.append([self.x,j])
+                break
+            else:
+                lst.append([self.x,j])
+
+        return lst
+
+class Bishop(Piece):
+    def __init__(self, x, y, color):
+        super().__init__(x, y, color, "bishop")
+
+    def legal_move(self,chess_grid):
+        lst = []
+        for i in range(8):
+            if 0 <= self.x+i <= 7 and 0 <= self.y+i <= 7 and i != self.x and i != self.y:
+                if chess_grid[self.x+i][self.y+i]:
+                    if chess_grid[self.x+i][self.y+i].color != self.color:
+                        lst.append([self.x+i,self.y+i])
+                    break
+                else:
+                    lst.append([self.x+i,self.y+i])
+
+        for i in range(8):
+            if 0 <= self.x-i <= 7 and 0 <= self.y-i <= 7 and i != self.x and i != self.y:
+                if chess_grid[self.x-i][self.y-i]:
+                    if chess_grid[self.x-i][self.y-i].color != self.color:
+                        lst.append([self.x-i,self.y-i])
+                    break
+                else:
+                    lst.append([self.x-i,self.y-i])
+
+        for i in range(8):
+            if 0 <= self.x+i <= 7 and 0 <= self.y-i <= 7 and i != self.x and i != self.y:
+                if chess_grid[self.x+i][self.y-i]:
+                    if chess_grid[self.x+i][self.y-i].color != self.color:
+                        lst.append([self.x+i,self.y-i])
+                    break
+                else:
+                    lst.append([self.x+i,self.y-i])
+
+        for i in range(8):
+            if 0 <= self.x-i <= 7 and 0 <= self.y+i <= 7 and i != self.x and i != self.y:
+                if chess_grid[self.x-i][self.y+i]:
+                    if chess_grid[self.x-i][self.y+i].color != self.color:
+                        lst.append([self.x-i,self.y+i])
+                    break
+                else:
+                    lst.append([self.x-i,self.y+i])
+
+        return lst
+
+class Queen(Piece):
+    def __init__(self, x, y, color):
+        super().__init__(x, y, color, "queen")
+
+    def legal_move(self,chess_grid):
+
+
+
+class Knight(Piece):
+    def __init__(self, x, y, color):
+        super().__init__(x, y, color, "knight")
 
 class Pawn(Piece):
     def __init__(self, x, y, color):
